5a53a0c98e6135b753acc26a72032c96bb728116
dsang18 <darshilsanghvi1818@gmail.com>
2026-03-22 17:39:15 +0530

    Removed Credentials

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from config import settings
 
 db_url = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-# db_url = "postgresql://postgres:postgres@localhost:5432/chronic"
+
 engine = create_engine(db_url)
 SessionLocale = sessionmaker(autoflush=False,autocommit=False, bind=engine)
 
